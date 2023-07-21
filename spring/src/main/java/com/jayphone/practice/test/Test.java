0a5dbaae5a39cc059f47cf195b4b06099e286d7b
Jayphone <354297098@qq.com>
2023-07-22 00:26:00 +0800

    feat:提交

--- spring/src/main/java/com/jayphone/practice/test/Test.java
+++ spring/src/main/java/com/jayphone/practice/test/Test.java
@@ -7,8 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        Student student = (Student) applicationContext.getBean("student");
-        System.out.println(student);
-
+//        Student student = (Student) applicationContext.getBean("student");
+//        System.out.println(student);
+        Student student2 = (Student) applicationContext.getBean("student2");
+        System.out.println(student2);
     }
 }
